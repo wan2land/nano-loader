e3767156cf4b8529f8dd1095872bc418a6c2c2c8
Changwan Jun <wan2land@gmail.com>
2021-07-01 02:34:44 +0900

    fix type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 
-export declare function defer<T>(createLoad: () => T | Promise<T>): Promise<T>
+export declare function defer<T>(createLoad: () => T | Promise<T>): () => Promise<T>
 
 export declare function load(src: string, options?: Partial<Omit<HTMLScriptElement, keyof HTMLElement | 'src' | 'async'>>): Promise<undefined>
