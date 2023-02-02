bae0b9348442bacba9475ce16a4d352931e8c24c
Changwan Jun <wan2land@gmail.com>
2023-02-02 11:23:19 +0900

    fix: doc, test

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Nano Loader
 
 <p>
+  <a href="https://www.npmjs.com/package/nano-loader"><img alt="Version" src="https://img.shields.io/npm/v/nano-loader.svg?style=flat-square&logo=npm" /></a>
   <a href="https://npmcharts.com/compare/nano-loader?minimal=true"><img alt="Downloads" src="https://img.shields.io/npm/dt/nano-loader.svg?style=flat-square" /></a>
-  <a href="https://www.npmjs.com/package/nano-loader"><img alt="Version" src="https://img.shields.io/npm/v/nano-loader.svg?style=flat-square" /></a>
   <a href="https://www.npmjs.com/package/nano-loader"><img alt="License" src="https://img.shields.io/npm/l/nano-loader.svg?style=flat-square" /></a>
 </p>
+
+Dynamic Script Loader for Browser, enabling dynamic script loading from any URL.
 
 ## Installation
 
@@ -14,7 +16,9 @@ npm install nano-loader --save
 
 ## Usage
 
-**Load a script file.**
+`load`
+
+The load method loads a script from the given URL.
 
 ```ts
 import { load } from 'nano-loader'
@@ -24,12 +28,14 @@ await load('https://something.com/foo.js')
 console.log(someFunctionFromFoo()) // work!
 ```
 
-**Defer load**
+`once`
+
+The once method ensures that the script is loaded only once, no matter how many times it is called.
 
 ```js
-import { defer, load } from 'nano-loader'
+import { once, load } from 'nano-loader'
 
-const loadFoo = defer(() => load('https://something.com/foo.js'))
+const loadFoo = once(() => load('https://something.com/foo.js'))
 
 await Promise.all([...Array(10000).keys()].map(() => loadFoo())) // load 10000times, but load script once.
 
@@ -41,12 +47,13 @@ console.log(someFunctionFromFoo())
 ### Use with Kakao Map + Vue
 
 ```js
-import { defer, load } from 'nano-loader'
+import { once, load } from 'nano-loader'
 
 const KAKAO_KEY = process.env.KAKAO_KEY
 
-const loadDaumMapSdk = defer(
-  () => load(`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_KEY}&libraries=services&autoload=false`).then(() => new Promise((resolve) => window.kakao.maps.load(resolve)))
+const loadDaumMapSdk = once(
+  () => load(`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_KEY}&libraries=services&autoload=false`)
+    .then(() => new Promise((resolve) => window.kakao.maps.load(resolve)))
 )
 
 export default {
@@ -57,3 +64,4 @@ export default {
 }
 
 ```
+
---
+++ src/loader.test.ts
@@ -0,0 +1,181 @@
+import { once } from './loader'
+
+describe('nano-loader, once', () => {
+  it("test once sync success", async () => {
+    const mock = jest.fn()
+    const load = once(() => {
+      mock()
+      return true
+    })
+
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+
+    expect(mock.mock.calls.length).toBe(1)
+  })
+
+  it("test once sync promise success", async () => {
+    const mock = jest.fn()
+    const load = once(() => {
+      mock()
+      return Promise.resolve(true)
+    })
+
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+
+    expect(mock.mock.calls.length).toBe(1)
+  })
+
+  it("test once async success", async () => {
+    const mock = jest.fn()
+    const load = once(async () => {
+      mock()
+      return true
+    })
+
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+    expect(await load()).toBe(true)
+
+    expect(mock.mock.calls.length).toBe(1)
+  })
+
+  it("test once sync error", async () => {
+    const mock = jest.fn()
+    const load = once(() => {
+      mock()
+      throw new Error('test')
+    })
+
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+
+    expect(mock.mock.calls.length).toBe(4)
+  })
+
+  it("test once sync promise error", async () => {
+    const mock = jest.fn()
+    const load = once(() => {
+      mock()
+      return Promise.reject(new Error('test'))
+    })
+
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+
+    expect(mock.mock.calls.length).toBe(4)
+  })
+
+  it("test once async error", async () => {
+    const mock = jest.fn()
+    const load = once(async () => {
+      mock()
+      throw new Error('test')
+    })
+
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+    await expect(load()).rejects.toThrow(new Error('test'))
+
+    expect(mock.mock.calls.length).toBe(4)
+  })
+
+  it("test once async lazy error", async () => {
+    const mock = jest.fn()
+
+    let resume = () => {}
+    const purge = () => new Promise<void>((resolve) => { resume = resolve })
+
+    const load = once(async () => {
+      mock()
+      await purge()
+      throw new Error('test')
+    })
+
+    let lazyCases = [] as Promise<void>[]
+
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+
+    await new Promise((resolve) => setTimeout(resolve, 100))
+
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+    lazyCases.push(expect(load()).rejects.toThrow(new Error('test')))
+
+    resume()
+
+    await Promise.all(lazyCases)
+
+    expect(mock.mock.calls.length).toBe(1)
+  })
+
+  it("test once async lazy error with success", async () => {
+    const mock = jest.fn()
+
+    let resume = () => {}
+    let success = false
+    const purge = () => new Promise<void>((resolve) => { resume = resolve })
+
+    const load = once(async () => {
+      mock()
+      await purge()
+      if (success) return true
+      throw new Error('test')
+    })
+
+    {
+      const lazyLoads = [] as Promise<boolean>[]
+
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+
+      resume()
+
+      await Promise.all(lazyLoads).catch(() => { /* ignore */ })
+
+      expect(mock.mock.calls.length).toBe(1)
+    }
+
+    {
+      const lazyLoads = [] as Promise<boolean>[]
+
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      lazyLoads.push(load())
+      await new Promise((resolve) => setTimeout(resolve, 100))
+
+      success = true
+      resume()
+
+      expect(await Promise.all(lazyLoads)).toEqual([true, true, true, true])
+
+      expect(mock.mock.calls.length).toBe(2)
+    }
+  })
+})
